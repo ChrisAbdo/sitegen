c49b3b17cfbfbce3159846163e221827041c92f3
Chris Abdo <chris.j.abdo@gmail.com>
2025-07-20 00:41:34 -0400

    system prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -9,6 +9,8 @@ export async function POST(req: Request) {
 
   const result = streamText({
     model: google("gemini-2.5-flash"),
+    system:
+      "You are an assistant that generates websites in only 1 html file with bootstrap for styling. You are going to receive a message input that is the request from the user (ex. 'I want a website for my restaurant...')... base your website on that. Output only the code and nothing else.",
     messages: convertToModelMessages(messages),
   });
 
